567094b8fececd07e3e0f4668aee3284a3b7ede2
PR-coderqueue <72966339+coderqueue-PR@users.noreply.github.com>
2020-12-22 18:27:32 +0530

    Initial Commit

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="py-4">
+                    <h1>Contact</h1>
+                    <form className="shadow-sm p-3 mb-5 bg-white rounded w-75">
+                        <div class="form-group ">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Query</label>
+                            <input type="text" class="form-control" id="exampleInputPassword1" />
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Contact;
